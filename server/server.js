2e339396d2b258efe2b5bebe779f27677d95c631
Subitha2005 <k.subitha.83@gmail.com>
2026-04-19 23:55:37 +0530

    another update in server.js for deploy

--- server/server.js
+++ server/server.js
@@ -55,10 +55,10 @@ if (!MONGO_URI) {
 
 await connectDB(MONGO_URI);
 
-if(process.env.NODE_ENV === "production") {
-    const PORT = process.env.PORT  || 5000;
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        console.log(`Local server running on port ${PORT}`);
     });
 }
 
